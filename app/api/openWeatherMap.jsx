41a41de2065e015e4532d3285ac82b268f72bdcd
someoneGuyILuz <you@example.com>
2017-03-05 21:09:45 +0200

    del debuger

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,7 +9,6 @@ getTemp: function (location) {
   var encodedLocation= encodeURIComponent(location)
   var requesUrl=`${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
    return axios.get(requesUrl).then(function(res){
-     debugger;
   if(res.data.cod&& res.data.message){
   throw new Error(res.data.message);
 }else{
